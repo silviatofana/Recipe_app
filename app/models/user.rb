40ac964e0f0d555e68b6f61a64d4b50ae91534ff
silviatofana <silvia.tofana@gmail.com>
2022-10-05 18:04:01 +0300

    linters

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< HEAD
-=======
 
   has_many :foods
   has_many :recipes, foreign_key: 'user_id', class_name: 'Recipe'
->>>>>>> 38ac739680ed8efea405d95ad9337f723384fb31
 end
