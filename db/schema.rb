0e0f0a3149ee7a053c5135540dd2698f9cfcfca8
silviatofana <silvia.tofana@gmail.com>
2022-10-05 22:05:09 +0300

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 20_220_830_160_112) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_070735) do
->>>>>>> 38ac739680ed8efea405d95ad9337f723384fb31
   # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
-<<<<<<< HEAD
   create_table 'foods', force: :cascade do |t|
     t.string 'name'
     t.string 'measurement_unit'
@@ -27,30 +22,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_070735) do
     t.datetime 'updated_at', null: false
     t.bigint 'users_id', null: false
     t.index ['users_id'], name: 'index_foods_on_users_id'
-=======
-  create_table "foods", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "measurement", null: false
-    t.integer "price", null: false
-    t.integer "quantity", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_foods_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
->>>>>>> 38ac739680ed8efea405d95ad9337f723384fb31
   end
 
   create_table 'recipe_foods', force: :cascade do |t|
