cf1c4f5ed849c80e47294be0b94b87cae5661a96
silviatofana <silvia.tofana@gmail.com>
2022-10-05 16:02:49 +0300

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  root 'users#index'
   devise_for :users
   resources :users
+  resources :foods
+  resources :recipes, only: %i[index show new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'home#index'
+  resources :recipes do
+    # resources :recipe_food, only: [:show, :new, :create, :edit, :update, :destroy]
+    member do
+      get :delete
+    end
+  end
 end
