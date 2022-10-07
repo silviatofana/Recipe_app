ede49f81ec54a95c90c13793fa13bcd5de176e9a
ABDULALI3468 <mrabdulali2003@gmail.com>
2022-10-07 19:55:31 +0500

    Wrote tests for Recipes.

---
+++ spec/models/recipe_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Recipe, type: :model do
+  # Test associations.
+  describe 'associations' do
+    it 'has many RecipeFood' do
+      association = described_class.reflect_on_association(:recipe_foods)
+      expect(assc.macro).to eq :has_many
+    end
+
+    it 'belongs to User' do
+      assc = described_class.reflect_on_association(:user)
+      expect(association.macro).to eq :belongs_to
+    end
+  end
+end
